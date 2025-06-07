95f45c37b56fbbc84aa7695364ef08d3641bcbfc
Mathmagician8191 <50558333+Mathmagician8191@users.noreply.github.com>
2025-06-08 00:25:50 +1200

    Switch solar system data to json

--- celestial_bodies.py
+++ celestial_bodies.py
@@ -1,9 +1,37 @@
-from config import system
+import json
+import os
 
-match system:
-  case "rss":
-    from rss_bodies import *
-  case "ksp":
-    from ksp_bodies import *
-  case x:
-    print(f"Invalid system {x}")
+SYSTEM = "rss"
+SCALE = 1
+DAY_SCALE = 1
+LOW_ORBIT = 200
+ORBIT_ESTIMATE = 9400
+
+# Indices
+RADIUS = 0
+GRAVITY = 1
+GM = 2
+ROTATION = 3
+PARENT = 4
+HEIGHT = 5
+
+def load_planet(radius, gravity, rotation, parent, height):
+  radius *= SCALE
+  rotation *= SCALE / DAY_SCALE
+  gm = radius ** 2 * 1000 * gravity
+  return (radius, gravity, gm, rotation, parent, height)
+
+filepath = f"solar-systems/{SYSTEM}.json"
+
+if not os.path.isfile(filepath):
+  print(f"Invalid solar system: {SYSTEM}")
+  quit()
+
+systems = {}
+
+with open(filepath) as file:
+  data = json.load(file)
+  for body in data["bodies"]:
+    name = body["name"]
+    systems[name] = load_planet(body["radius"], body["gravity"], body["rotation"], body["parent"], body["height"])
+  DEFAULT = systems[data["default"]]
